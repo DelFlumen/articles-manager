808e57134fb46a4aa5d3d0e11b26a9b59ced81ef
Yaroslav Zaychenko <delflumen@ukr.net>
2024-03-17 11:40:55 +0200

    feat: add throttler

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -10,7 +10,9 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Prisma } from '@prisma/client';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -20,11 +22,13 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @SkipThrottle({ default: false })
   @Get()
   findAll(@Query('sort') sort?: 'name', @Query('type') type?: 'asc' | 'desc') {
     return this.usersService.findAll(sort, type);
   }
 
+  @Throttle({ short: { ttl: 1000, limit: 1 } })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
